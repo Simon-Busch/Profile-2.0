a8194b67bba197b1dd3543798d5459d32dbd24a6
Simon Busch <simon.busch@live.fr>
2020-11-21 09:49:18 +0100

    tests / improvements

---
+++ javascript/typed.js
@@ -0,0 +1,15 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Change your life", "Learn to code"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
+
+document.addEventListener('turbolinks:load', () => {
+  loadDynamicBannerText();
+});
